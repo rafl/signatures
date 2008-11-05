f168e0a59c17aeddee18f4e33677d0781cda08c3
Florian Ragwitz <rafl@debian.org>
2008-11-05 02:21:28 +0100

    Documentation for the methods intended to be overwritten.

--- lib/Sub/Signature.pm
+++ lib/Sub/Signature.pm
@@ -100,6 +100,28 @@ Sub::Signature - subroutine signatures with no source filter
         return $bar + $baz;
     }
 
+=head1 METHODS
+
+=head2 proto_unwrap ($prototype)
+
+Turns the extracted C<$prototype> into code.
+
+The default implementation returns C<< my (${prototype}) = @_; >> or an empty
+string, if no prototype is given.
+
+=head2 inject ($offset, $code)
+
+Inserts a C<$code> string into the line perl currently parses at the given
+C<$offset>.
+
+=head2 callback (offset, $prototype)
+
+This gets called as soon as a sub definition is encountered. Arguments are the
+C<$offset> within the current line perl is parsing and extracted C<$prototype>.
+
+The default implementation calls C<proto_unwrap> with the prototype and passes
+the returned value and the offset to C<inject>.
+
 =head1 AUTHOR
 
 Florian Ragwitz E<lt>rafl@debian.orgE<gt>
